bfb1f578267b751165c74c8e4462b19563554c74
upatra <upatra@XE-T-UMAS.XEBIAINDIA.LOCAL>
2019-01-28 21:32:30 +0400

    Method added

--- interview_programming/src/interview_programming/Test.java
+++ interview_programming/src/interview_programming/Test.java
@@ -2,7 +2,14 @@ package interview_programming;
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("Hello ti ok uyoyy");
+	System.out.println("Hello  uyoyy");
+	
+	m1();
+	
+}
+
+private static void m1() {
+	// TODO Auto-generated method stub
 	
 }
 }
